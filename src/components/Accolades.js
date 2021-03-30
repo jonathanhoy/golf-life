93e3bc9b15486defc4c427122acc496300776fd0
jonathanhoy <jonathanhoy5@gmail.com>
2021-03-30 10:09:51 -0400

    updated Accolades styling

--- src/components/Accolades.js
+++ src/components/Accolades.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Card, CardHeading } from '../styles/Card';
+import variables from '../styles/variables';
 import { firebase, accolades_meta } from '../firebase';
 import { byParDifferential } from '../helper-functions/sortingFunctions';
 import { Link } from 'react-router-dom';
@@ -49,14 +50,18 @@ class Accolades extends React.Component {
             <h3>Best Courses</h3>
             <Course>
               {
-                this.state.course.map((item) => {
+                this.state.course.map((item, index) => {
                   return (
-                    <li className="course-item" key={item.data}>
-                      <p className="course-item-player">{item.player}</p>
-                      <p className="course-item-date">{item.date}</p>
+                    <li className={`course-item course-item-${index + 1}`} key={item.data}>
+                      <span>
+                        <p className="course-item-player">{item.player}</p>
+                        <p className="course-item-date">{item.date}</p>
+                      </span>
                       <img className="course-item-image" src={`./assets/maps/${item.image}.png`} alt={`${item.map} Course`}></img>
-                      <p className="course-item-map">{item.map}</p>
-                      <p className="course-item-score">{item.score} ({item.differential})</p>
+                      <span>
+                        <p className="course-item-map">{item.map}</p>
+                        <p className="course-item-score">{item.score} ({item.differential})</p>
+                      </span>
                     </li>
                   )
                 })
@@ -83,8 +88,19 @@ const Course = styled.ul`
   margin-top: 1rem;
   margin-bottom: 0;
   .course-item {
-    background: lightgrey;
     text-align: center;
+    &-1 {
+      background-color: #c1bfbf;
+      background-image: linear-gradient(315deg, #c1bfbf 0%, #af8231 74%);
+    }
+    &-2 {
+      background-color: #bdd4e7;
+      background-image: linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%);
+    }
+    &-3 {
+      background-color: #f5d3c8;
+      background-image: linear-gradient(315deg, #f5d3c8 0%, #c08552 74%);
+    }
     p {
       padding: 0 0.5rem;
       font-weight: 400;
@@ -95,6 +111,43 @@ const Course = styled.ul`
     img {
       display: block;
       width: 100%;
+    }
+  }
+  @media (max-width: ${variables.sm}) {
+    grid-template-columns: 1fr;
+    .course-item {
+      display: grid;
+      align-items: center;
+      &-1, &-3 {
+        grid-template-columns: 1fr 1fr 2fr;
+        span:first-of-type {
+          grid-column: 1 / 2;
+          grid-row: 1 / 2;
+        }
+        span:last-of-type {
+          grid-column: 2 / 3;
+          grid-row: 1 / 2;
+        }
+        img {
+          grid-column: 3 / 4;
+          grid-row: 1 / 2;
+        }
+      }
+      &-2 {
+        grid-template-columns: 2fr 1fr 1fr;
+        span:first-of-type {
+          grid-column: 2 / 3;
+          grid-row: 1 / 2;
+        }
+        span:last-of-type {
+          grid-column: 3 / 4;
+          grid-row: 1 / 2;
+        }
+        img {
+          grid-column: 1 / 2;
+          grid-row: 1 / 2;
+        }
+      }
     }
   }
 `;
