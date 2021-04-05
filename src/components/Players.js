6c691866bb0e99e2c9cd6bd3d48de9a1d805b2e8
jonathanhoy <jonathanhoy5@gmail.com>
2021-04-04 20:30:36 -0400

    added basic table for player info

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,43 +1,74 @@
 import React, { Component } from 'react';
-// import { Card, CardList } from '../styles/Card';
-// import Table from '../styles/Table';
+import { Card, CardList } from '../styles/Card';
+import Table from '../styles/Table';
 import Wrapper from '../styles/Wrapper';
 import PageHeading from '../styles/PageHeading';
-// import { firebase, meta } from '../firebase';
-// import { byLatestTourney } from '../sortingFunctions';
+import BodyText from '../styles/BodyText';
+import { firebase, playerData } from '../firebase';
+import { byWinPercentage } from '../helper-functions/sortingFunctions';
+import format from '../helper-functions/format';
 
 class Players extends Component {
   constructor() {
     super();
     this.state = {
-      meta: []
+      playerData: []
     }
   }
 
-  // componentDidMount() {
-  //   const dbRef = firebase.database().ref(`${meta}`);
-  //   dbRef.on('value', (response) => {
-  //     const data = response.val();
-  //     // Sort data to get latest tournament
-  //     const sortedData = data.sort(byLatestTourney);
-  //     let newMeta = [];
-  //     newMeta = [...sortedData];
-  //     // eslint-disable-next-line
-  //     const cleanedMeta = newMeta.filter((tourney) => {
-  //       if (tourney !== undefined && tourney.tourney_id >= 1) {
-  //         return tourney;
-  //       }
-  //     })
-  //     this.setState({
-  //       meta: cleanedMeta,
-  //     })
-  //   });
-  // }
+  componentDidMount() {
+    const dbRef = firebase.database().ref(`${playerData}`);
+    dbRef.on('value', (response) => {
+      const data = response.val();
+      // eslint-disable-next-line
+      const cleanedData = data.filter((player) => {
+        if (player !== undefined) {
+          player.avg_differential = format(Math.floor(player.avg_differential));
+          player.win_rate = player.win_rate.toFixed(3);
+          return player;
+        }
+      })
+      const sortedData = cleanedData.sort(byWinPercentage);
+      this.setState({
+        playerData: sortedData,
+      })
+    });
+  }
 
   render() {
     return (
       <Wrapper>
-        <PageHeading>🚧 Player data coming soon 🚧</PageHeading>
+        <PageHeading>Players</PageHeading>
+        <BodyText>Click on a player's name to see more information about that player.</BodyText>
+        <Card>
+        <Table className="maps">
+            <caption className="show-for-sr">Courses</caption>
+            <thead>
+              <tr>
+                <th><span>Player</span></th>
+                <th><span>Wins</span></th>
+                <th><span>Tournaments Played</span></th>
+                <th><span>Win %</span></th>
+                <th><span>Average Margin</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.playerData.map((player) => {
+                  return (
+                    <tr key={player.id}>
+                      <td>{player.player}</td>
+                      <td>{player.total_wins}</td>
+                      <td>{player.games_played}</td>
+                      <td>{player.win_rate}</td>
+                      <td>{player.avg_differential}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </Card>
       </Wrapper>
     )
   }
